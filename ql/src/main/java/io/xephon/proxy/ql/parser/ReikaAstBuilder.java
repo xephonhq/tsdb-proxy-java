ac7530435fd36191bc38e3b8933925ed33eb4c6f
at15 <at15@outlook.com>
2017-03-04 15:28:42 -0800

    [ql][parser][ast] Add more override method
    - [ ] TODO: seems it is better to let the visitor return Exp

--- ql/src/main/java/io/xephon/proxy/ql/parser/ReikaAstBuilder.java
+++ ql/src/main/java/io/xephon/proxy/ql/parser/ReikaAstBuilder.java
@@ -19,6 +19,32 @@ public class ReikaAstBuilder extends ReikaBaseVisitor<Node> {
     @Override
     public Node visitVarDeclareStat(ReikaParser.VarDeclareStatContext ctx) {
         System.out.println("visit var declare statement");
+        System.out.println(ctx.varDeclare().type().getText());
+        System.out.println(ctx.varDeclare().ID().getText());
+        System.out.println(ctx.varDeclare().expr().getText());
+        System.out.println("line:" + ctx.varDeclare().getStart().getLine());
+        return visitChildren(ctx);
+    }
+
+    @Override
+    public Node visitVarAssignStat(ReikaParser.VarAssignStatContext ctx) {
+        System.out.println("visit var assign statement");
+        System.out.println(ctx.varAssign().ID().getText());
+        System.out.println(ctx.varAssign().expr().getText());
+        return visitChildren(ctx);
+    }
+
+    @Override
+    public Node visitExprStat(ReikaParser.ExprStatContext ctx) {
+        System.out.println("visit expr statement");
+        System.out.println(ctx.expr().getText());
+        return visitChildren(ctx);
+    }
+
+    @Override
+    public Node visitCall(ReikaParser.CallContext ctx) {
+        System.out.println("visit call expression");
+//        ctx.exprList().expr(); // this will return a list
         return visitChildren(ctx);
     }
 }
