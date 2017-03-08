db6d08904c99edfd5be9fe9b6fc4b02e26a6ba1d
Chujiao <chou8@ucsc.edu>
2017-03-08 15:55:26 -0800

    [ast][BoolLiteral] #8 add class for bool literal

---
+++ ql/src/main/java/io/xephon/proxy/ql/ast/BoolLiteral.java
@@ -0,0 +1,10 @@
+package io.xephon.proxy.ql.ast;
+
+/**
+ * Created by summer2016 on 3/8/17.
+ */
+public class BoolLiteral extends BoolExp {
+    public final boolean bool;
+
+    public BoolLiteral(boolean b) { bool = b; }
+}
