23ef5b5350e5e5454a060b14177366fa332d31a3
Chujiao <chou8@ucsc.edu>
2017-03-08 17:32:41 -0800

    [test][Interpreter] add test for bool interpreter
    but I'm not very sure about it although test passes

--- ql/src/test/java/io/xephon/proxy/ql/interpreter/NaiveInterpreterTest.java
+++ ql/src/test/java/io/xephon/proxy/ql/interpreter/NaiveInterpreterTest.java
@@ -44,4 +44,23 @@ public class NaiveInterpreterTest {
         assertEquals(10.0, (double) interpreter.resolveDouble("a"),0.0);
     }
 
+    @Test
+    public void testBool() throws Exception {
+        ReikaAstBuilder astBuilder = Util.astBuilderFromResource("bool_only2.reika");
+//        astBuilder.printErrors();
+        assertFalse(astBuilder.hasError());
+        NaiveInterpreter interpreter = new NaiveInterpreter();
+        interpreter.evalProgram(astBuilder.getStatements());
+        //assertEquals(10.0, (double) interpreter.resolveDouble("a"),0.0);
+
+        //confused about these two
+        assertFalse((boolean) interpreter.resolveBool("d"));
+        assertFalse("d",false);
+
+        assertTrue("c",true);
+        //assertFalse("g",false);
+        assertFalse("a", false);
+        assertFalse("b",false);
+    }
+
 }
